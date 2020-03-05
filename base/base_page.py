c8117e48ccabeb44326b57b5025cd737ca709bf3
2623709006 <2623709006@qq.com>
2020-03-05 11:16:43 +0800

    Initial commit

---
+++ base/base_page.py
@@ -0,0 +1,32 @@
+"""
+PO文件基类
+"""
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element_func(self, location, timeout=5, poll=.5):
+        """
+        元素定位方法
+        :param location: 定位信息
+        :param timeout: 超时时长
+        :param poll: 方法执行间隔
+        :return: 元素对象
+        """
+        element = WebDriverWait(self.driver, timeout=timeout, poll_frequency=poll). \
+            until(lambda x: x.find_element(location[0], location[1]))
+        return element
+
+    def click_func(self, location):
+        """元素点击方法"""
+        element = self.find_element_func(location)  # 定位目标元素
+        element.click()  # 调用点击方法
+
+    def input_func(self, location, text):
+        """元素输入方法"""
+        element = self.find_element_func(location)  # 定位目标元素
+        element.clear()  # 调用清空方法
+        element.send_keys(text)  # 调用输入方法
---
+++ utils.py
@@ -0,0 +1,27 @@
+"""
+驱动获取方法
+"""
+from appium import webdriver
+
+
+def get_driver():
+    """驱动对象获取方法"""
+    # 获取驱动对象
+    capabilities = {
+        "platformName": "Android",  # 测试平台（Android、iOS）
+        "platformVersion": "5.1",  # 测试系统版本
+        "deviceName": "模拟器",  # 测试设备名称
+        "appPackage": "com.bjcsxq.chat.carfriend",  # 待测应用的包名
+        "appActivity": ".MainActivity",  # 待测应用的启动名
+        # 输入中文设置
+        "resetKeyboard": True,
+        "unicodeKeyboard": True
+    }
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
+    return driver
+
+
+if __name__ == '__main__':
+    get_driver()
+
+# 这道题太难了 俺不会做
